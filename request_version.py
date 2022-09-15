ba546636e61d5476826d70f25f46b3d4d5bf1820
Joshua Nam <60495431+JoshuaN18@users.noreply.github.com>
2022-09-14 18:19:29 -0600

    Update request_version.py

--- request_version.py
+++ request_version.py
@@ -2,3 +2,5 @@ import requests
 
 res = requests.get("http://www.google.com/")
 print(res.text)
+res1 = requests.get("https://raw.githubusercontent.com/JoshuaN18/CMPUT-404-Labs/main/request_version.py")
+print(res1.text)
